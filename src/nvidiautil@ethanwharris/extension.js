67c19c25dfb47739ac48599dc0dbcf17f56e189b
MattPainter01 <mp2u16@soton.ac.uk>
2017-10-30 15:42:44 +0000

    Added support for nvidia-settings use if smi fails

--- src/nvidiautil@ethanwharris/extension.js
+++ src/nvidiautil@ethanwharris/extension.js
@@ -14,6 +14,8 @@ var util_label;
 var temp_label;
 var mem_label;
 
+var use_nvidia_settings = true;
+
 function init() {
   Gtk.IconTheme.get_default().append_search_path(Me.dir.get_child('icons').get_path());
 }
@@ -68,7 +70,14 @@ function open_settings() {
 }
 
 function get_info() {
+  if (!use_nvidia_settings) {
+    return get_info_smi();
+  } else {
+    return get_info_settings();
+  }
+}
 
+function get_info_smi() {
   var smi = GLib.spawn_command_line_sync("nvidia-smi")[1].toString().split('\n');
 
   var values_line = smi[8];
@@ -82,7 +91,7 @@ function get_info() {
   for (var i = 0; i < values_line.length; i++) {
       var c = values_line.charAt(i);
 
-      if ((c != " " && c != "|" && c != "/") || (c == "/" && buffer_index > 0)) {
+      if (c >= '0' && c <= '9') {
         buffer_state = true;
         buffer[buffer_index] = c;
         buffer_index = buffer_index + 1;
@@ -95,15 +104,36 @@ function get_info() {
       }
   }
 
-  var temp = values[1]; temp = temp.substring(0, temp.length-1);
-  var used_memory = values[5]; used_memory = used_memory.substring(0, used_memory.length-3);
-  var total_memory = values[6];total_memory = total_memory.substring(0, total_memory.length-3);
-  var util = values[7]; util = util.substring(0, util.length-1);
+  if (values.length < 8) {
+    use_nvidia_settings = true;
 
-  var mem_usage = (used_memory / total_memory * 100).toString();
-  mem_usage = mem_usage.substring(0,2);
+  } else {
+    var temp = values[1];
+    var used_memory = values[5];
+    var total_memory = values[6];
+    var util = values[7];
 
-  return [util, temp, mem_usage];
+    var mem_usage = (used_memory / total_memory * 100).toString();
+    mem_usage = mem_usage.substring(0,2);
+
+    return [util, temp, mem_usage];
+  }
+}
+
+function get_info_settings() {
+   var util = GLib.spawn_command_line_sync("nvidia-settings -q GPUUtilization -t")[1].toString();
+   util = util.substring(9,11);
+   util = util.replace(/\D/g,'');
+
+   var temp = GLib.spawn_command_line_sync("nvidia-settings -q GPUCoreTemp -t")[1].toString();
+   temp = temp.split('\n')[0];
+
+   var used_memory = GLib.spawn_command_line_sync("nvidia-settings -q UsedDedicatedGPUMemory -t")[1];
+   var total_memory = GLib.spawn_command_line_sync("nvidia-settings -q TotalDedicatedGPUMemory -t")[1];
+   var mem_usage = (used_memory / total_memory * 100).toString();
+   mem_usage = mem_usage.substring(0,2);
+
+   return [util, temp, mem_usage];
 }
 
 function build_button_box(info) {
